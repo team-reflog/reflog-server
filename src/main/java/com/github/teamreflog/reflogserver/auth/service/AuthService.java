7b3d003bdbc34ba1c3a2bb13b50a8bb0c711e934
whatasame <97666463+whatasame@users.noreply.github.com>
2023-12-31 18:11:22 +0900

    feat: 일치하지 않는 비밀번호 검증

--- src/main/java/com/github/teamreflog/reflogserver/auth/service/AuthService.java
+++ src/main/java/com/github/teamreflog/reflogserver/auth/service/AuthService.java
@@ -2,6 +2,8 @@ package com.github.teamreflog.reflogserver.auth.service;
 
 import com.github.teamreflog.reflogserver.auth.dto.LoginRequest;
 import com.github.teamreflog.reflogserver.auth.exception.EmailNotExistException;
+import com.github.teamreflog.reflogserver.auth.exception.PasswordNotMatchedException;
+import com.github.teamreflog.reflogserver.member.domain.Member;
 import com.github.teamreflog.reflogserver.member.domain.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,8 +15,13 @@ public class AuthService {
     private final MemberRepository memberRepository;
 
     public void login(final LoginRequest request) {
-        memberRepository
-                .findByEmail(request.getMemberEmail())
-                .orElseThrow(EmailNotExistException::new);
+        final Member member =
+                memberRepository
+                        .findByEmail(request.getMemberEmail())
+                        .orElseThrow(EmailNotExistException::new);
+
+        if (!member.isMatchedPassword(request.password())) {
+            throw new PasswordNotMatchedException();
+        }
     }
 }
