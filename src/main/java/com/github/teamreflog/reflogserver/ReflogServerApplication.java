8cbaeda1115d3064d949547eba90eb662ce6283f
whatasame <97666463+whatasame@users.noreply.github.com>
2023-12-31 14:27:11 +0900

    Initial commit

---
+++ src/main/java/com/github/teamreflog/reflogserver/ReflogServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.teamreflog.reflogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReflogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReflogServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/teamreflog/reflogserver/ReflogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.teamreflog.reflogserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReflogServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
