3baa160d8adeab34d8e01c4badf8aa6fe3808f46
rimrim990 <lim990718@naver.com>
2023-12-31 18:35:54 +0900

    feat: 테스트 데이터 초기화

--- src/test/java/com/github/teamreflog/reflogserver/acceptance/AcceptanceTest.java
+++ src/test/java/com/github/teamreflog/reflogserver/acceptance/AcceptanceTest.java
@@ -4,7 +4,10 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
+@Sql(scripts = "/clear.sql", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class AcceptanceTest {
 
