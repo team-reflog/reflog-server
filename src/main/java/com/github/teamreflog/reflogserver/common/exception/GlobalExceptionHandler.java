f90902acb44286497a106f492db8417a3dd50741
whatasame <97666463+whatasame@users.noreply.github.com>
2024-01-01 13:38:18 +0900

    feat: 글로벌 예외 상태 코드 추가

--- src/main/java/com/github/teamreflog/reflogserver/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/teamreflog/reflogserver/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.github.teamreflog.reflogserver.common.exception;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -7,7 +10,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
-    public ErrorResponse handleRuntimeException(final RuntimeException e) {
-        return new ErrorResponse("일시적인 에러가 발생했습니다. 잠시 후 다시 시도해주세요.");
+    public ResponseEntity<ErrorResponse> handleRuntimeException(final RuntimeException e) {
+
+        return new ResponseEntity<>(
+                new ErrorResponse("일시적인 에러가 발생했습니다. 잠시 후 다시 시도해주세요."), INTERNAL_SERVER_ERROR);
     }
 }
