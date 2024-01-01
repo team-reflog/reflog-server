5ba317447a8988178e908925a9157c92623d9713
whatasame <97666463+whatasame@users.noreply.github.com>
2024-01-01 12:20:55 +0900

    feat: CORS 설정 추가

---
+++ src/main/java/com/github/teamreflog/reflogserver/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.github.teamreflog.reflogserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .maxAge(3600L);
+    }
+}
