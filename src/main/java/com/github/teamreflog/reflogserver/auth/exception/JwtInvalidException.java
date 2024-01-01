fa1f9957f82d62e6f7974c1d240d11cbcb6b3fb6
whatasame <97666463+whatasame@users.noreply.github.com>
2024-01-01 13:38:31 +0900

    refactor: static import 추가

--- src/main/java/com/github/teamreflog/reflogserver/auth/exception/JwtInvalidException.java
+++ src/main/java/com/github/teamreflog/reflogserver/auth/exception/JwtInvalidException.java
@@ -1,16 +1,16 @@
 package com.github.teamreflog.reflogserver.auth.exception;
 
-import org.springframework.http.HttpStatus;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 public class JwtInvalidException extends AuthException {
 
     private static final String MESSAGE = "유효하지 않은 토큰입니다.";
 
     public JwtInvalidException() {
-        super(HttpStatus.BAD_REQUEST, MESSAGE);
+        super(BAD_REQUEST, MESSAGE);
     }
 
     public JwtInvalidException(final Throwable cause) {
-        super(HttpStatus.BAD_REQUEST, MESSAGE, cause);
+        super(BAD_REQUEST, MESSAGE, cause);
     }
 }
--- src/main/java/com/github/teamreflog/reflogserver/auth/exception/PasswordNotMatchedException.java
+++ src/main/java/com/github/teamreflog/reflogserver/auth/exception/PasswordNotMatchedException.java
@@ -1,12 +1,12 @@
 package com.github.teamreflog.reflogserver.auth.exception;
 
-import org.springframework.http.HttpStatus;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 public class PasswordNotMatchedException extends AuthException {
 
     private static final String MESSAGE = "비밀번호가 일치하지 않습니다.";
 
     public PasswordNotMatchedException() {
-        super(HttpStatus.BAD_REQUEST, MESSAGE);
+        super(BAD_REQUEST, MESSAGE);
     }
 }
