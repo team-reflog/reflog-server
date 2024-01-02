315b5d70743b97feccf27146add9e9018ce4fe1f
rimrim990 <lim990718@naver.com>
2024-01-02 20:21:30 +0900

    test: Invite fixture 생성

---
+++ src/test/java/com/github/teamreflog/reflogserver/acceptance/fixture/InviteFixture.java
@@ -0,0 +1,28 @@
+package com.github.teamreflog.reflogserver.acceptance.fixture;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import com.github.teamreflog.reflogserver.team.dto.TeamInvitationRequest;
+import io.restassured.RestAssured;
+
+public abstract class InviteFixture {
+
+    private InviteFixture() {}
+
+    public static void inviteMember(
+            final String accessToken, final String memberEmail, final Long teamId) {
+        RestAssured.given()
+                .log()
+                .all()
+                .auth()
+                .oauth2(accessToken)
+                .body(new TeamInvitationRequest(memberEmail, teamId))
+                .contentType(APPLICATION_JSON_VALUE)
+                .when()
+                .post("/invites")
+                .then()
+                .log()
+                .all()
+                .statusCode(200);
+    }
+}
