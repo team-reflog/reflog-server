2bc4e11ce1fd8a796ec561f8e5adc7b10f460931
Jeonggyu Choi <97666463+whatasame@users.noreply.github.com>
2024-01-05 20:32:06 +0900

    feat: 인수 테스트에서 500 에러 로그 확인할 수 있도록 설정 (#29)

--- src/main/java/com/github/teamreflog/reflogserver/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/teamreflog/reflogserver/common/exception/GlobalExceptionHandler.java
@@ -2,19 +2,22 @@ package com.github.teamreflog.reflogserver.common.exception;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @Order(Ordered.LOWEST_PRECEDENCE)
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponse> handleRuntimeException(final RuntimeException e) {
-        // TODO: logging cause
+        log.error("Unexpected error processing request", e);
+
         return new ResponseEntity<>(
                 new ErrorResponse("일시적인 에러가 발생했습니다. 잠시 후 다시 시도해주세요."), INTERNAL_SERVER_ERROR);
     }
