a569675dfba1edb90d315c14d35d7e96659d3444
whatasame <97666463+whatasame@users.noreply.github.com>
2024-01-07 01:22:18 +0900

    fix: 회고일을 추가해서 조회하도록 변경

--- src/main/java/com/github/teamreflog/reflogserver/reflection/application/dto/ReflectionCreateRequest.java
+++ src/main/java/com/github/teamreflog/reflogserver/reflection/application/dto/ReflectionCreateRequest.java
@@ -1,14 +1,20 @@
 package com.github.teamreflog.reflogserver.reflection.application.dto;
 
 import com.github.teamreflog.reflogserver.reflection.domain.Reflection;
+import java.time.LocalDate;
 
-public record ReflectionCreateRequest(Long memberId, Long topicId, String content) {
+public record ReflectionCreateRequest(
+        Long memberId, Long topicId, String content, String timezone) {
 
     public ReflectionCreateRequest setMemberId(final Long memberId) {
-        return new ReflectionCreateRequest(memberId, topicId, content);
+        return new ReflectionCreateRequest(memberId, topicId, content, timezone);
     }
 
-    public Reflection toEntity() {
-        return Reflection.of(memberId, topicId, content);
+    public ReflectionCreateRequest setTimezone(final String timezone) {
+        return new ReflectionCreateRequest(memberId, topicId, content, timezone);
+    }
+
+    public Reflection toEntity(final LocalDate localDate) {
+        return Reflection.of(memberId, topicId, content, localDate);
     }
 }
--- src/test/java/com/github/teamreflog/reflogserver/reflection/domain/DateProviderTest.java
+++ src/test/java/com/github/teamreflog/reflogserver/reflection/domain/DateProviderTest.java
@@ -4,8 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.LocalDate;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,32 +22,18 @@ class DateProviderTest {
     }
 
     @Test
-    @DisplayName("Timezone이 주어지면 오늘의 범위를 반환한다.")
-    void getTodayRangeTest() {
+    @DisplayName("Timezone이 주어지면 오늘 날짜를 반환한다.")
+    void getTodayOfZoneTest() {
         /* given */
         final Instant now = ZonedDateTime.of(2024, 1, 5, 4, 0, 0, 0, ZoneOffset.UTC).toInstant();
 
         /* when */
-        final DateRange seoulTodayRange = dateProvider.getTodayRange(now, "Asia/Seoul");
-        final DateRange newYorkTodayRange = dateProvider.getTodayRange(now, "America/New_York");
+        final LocalDate seoulToday = dateProvider.getTodayOfZone(now, "Asia/Seoul");
+        final LocalDate newYorkToday = dateProvider.getTodayOfZone(now, "America/New_York");
 
         /* then */
-        final LocalDateTime seoulStart =
-                ZonedDateTime.of(2024, 1, 5, 0, 0, 0, 0, ZoneId.of("Asia/Seoul")).toLocalDateTime();
-        final LocalDateTime seoulEnd =
-                ZonedDateTime.of(2024, 1, 5, 23, 59, 59, 999999999, ZoneId.of("Asia/Seoul"))
-                        .toLocalDateTime();
-        final LocalDateTime newYorkStart =
-                ZonedDateTime.of(2024, 1, 4, 0, 0, 0, 0, ZoneId.of("America/New_York"))
-                        .toLocalDateTime();
-        final LocalDateTime newYorkEnd =
-                ZonedDateTime.of(2024, 1, 4, 23, 59, 59, 999999999, ZoneId.of("America/New_York"))
-                        .toLocalDateTime();
-
         assertAll(
-                () -> assertThat(seoulTodayRange.start()).isEqualTo(seoulStart),
-                () -> assertThat(seoulTodayRange.end()).isEqualTo(seoulEnd),
-                () -> assertThat(newYorkTodayRange.start()).isEqualTo(newYorkStart),
-                () -> assertThat(newYorkTodayRange.end()).isEqualTo(newYorkEnd));
+                () -> assertThat(seoulToday).isEqualTo(LocalDate.of(2024, 1, 5)),
+                () -> assertThat(newYorkToday).isEqualTo(LocalDate.of(2024, 1, 4)));
     }
 }
