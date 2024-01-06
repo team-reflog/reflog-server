40798e1a3ef15746efdbea107c37299c26a7e718
Jeonggyu Choi <97666463+whatasame@users.noreply.github.com>
2024-01-06 22:36:35 +0900

    feat: 초대 거절 기능 추가 (#37)
    * refactor: 메서드 이름 오타 수정
    
    * feat: 초대 거절 기능 추가

--- src/main/java/com/github/teamreflog/reflogserver/team/application/InviteService.java
+++ src/main/java/com/github/teamreflog/reflogserver/team/application/InviteService.java
@@ -3,6 +3,7 @@ package com.github.teamreflog.reflogserver.team.application;
 import com.github.teamreflog.reflogserver.team.application.dto.InviteAcceptRequest;
 import com.github.teamreflog.reflogserver.team.application.dto.InviteCreateRequest;
 import com.github.teamreflog.reflogserver.team.application.dto.InviteQueryResponse;
+import com.github.teamreflog.reflogserver.team.application.dto.InviteRejectRequest;
 import com.github.teamreflog.reflogserver.team.domain.Crew;
 import com.github.teamreflog.reflogserver.team.domain.CrewRepository;
 import com.github.teamreflog.reflogserver.team.domain.Crews;
@@ -40,7 +41,7 @@ public class InviteService {
 
     public List<InviteQueryResponse> queryInvites(final Long memberId) {
         final List<InviteQueryResponse> responses = new ArrayList<>();
-        for (Invite invite : inviteRepository.findAllByMemberId(memberId)) {
+        for (final Invite invite : inviteRepository.findAllByMemberId(memberId)) {
             final Team team =
                     teamRepository
                             .findById(invite.getTeamId())
@@ -65,5 +66,15 @@ public class InviteService {
         final Crew crew = Crew.of(invite.getTeamId(), invite.getMemberId(), request.nickname());
         crews.add(crew);
         crewRepository.save(crew);
+    }
+
+    @Transactional
+    public void rejectInvite(final InviteRejectRequest inviteRejectRequest) {
+        final Invite invite =
+                inviteRepository
+                        .findById(inviteRejectRequest.inviteId())
+                        .orElseThrow(InviteNotExistException::new);
+        invite.reject(inviteRejectRequest.memberId());
+        inviteRepository.delete(invite);
     }
 }
--- src/test/java/com/github/teamreflog/reflogserver/team/domain/InviteTest.java
+++ src/test/java/com/github/teamreflog/reflogserver/team/domain/InviteTest.java
@@ -4,19 +4,61 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 
 import com.github.teamreflog.reflogserver.team.domain.exception.UnauthorizedInviteException;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("단위 테스트: Invite")
 class InviteTest {
 
-    @Test
-    @DisplayName("초대 대상 사용자와 요청 사용자가 일치하지 않는 경우 예외가 발생한다.")
-    void userInvalidThrowsException() {
-        /* given */
-        final Invite invite = Invite.of(1L, 1L);
+    @Nested
+    @DisplayName("초대를 수락할 때")
+    class AcceptInviteTest {
 
-        /* when & then */
-        assertThatCode(() -> invite.accept(2L))
-                .isExactlyInstanceOf(UnauthorizedInviteException.class);
+        @Test
+        @DisplayName("초대 대상 사용자와 요청 사용자가 일치하는 경우 예외가 발생하지 않는다.")
+        void userValid() {
+            /* given */
+            final Invite invite = Invite.of(1L, 1L);
+
+            /* when & then */
+            assertThatCode(() -> invite.accept(1L)).doesNotThrowAnyException();
+        }
+
+        @Test
+        @DisplayName("초대 대상 사용자와 요청 사용자가 일치하지 않는 경우 예외가 발생한다.")
+        void userInvalidThrowsException() {
+            /* given */
+            final Invite invite = Invite.of(1L, 1L);
+
+            /* when & then */
+            assertThatCode(() -> invite.accept(2L))
+                    .isExactlyInstanceOf(UnauthorizedInviteException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("초대를 거절할 때")
+    class RejectInviteTest {
+
+        @Test
+        @DisplayName("초대 대상 사용자와 요청 사용자가 일치하는 경우 예외가 발생하지 않는다.")
+        void userValid() {
+            /* given */
+            final Invite invite = Invite.of(1L, 1L);
+
+            /* when & then */
+            assertThatCode(() -> invite.reject(1L)).doesNotThrowAnyException();
+        }
+
+        @Test
+        @DisplayName("초대 대상 사용자와 요청 사용자가 일치하지 않는 경우 예외가 발생한다.")
+        void userInvalidThrowsException() {
+            /* given */
+            final Invite invite = Invite.of(1L, 1L);
+
+            /* when & then */
+            assertThatCode(() -> invite.reject(2L))
+                    .isExactlyInstanceOf(UnauthorizedInviteException.class);
+        }
     }
 }
