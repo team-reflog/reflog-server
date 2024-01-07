88effeb84a3b8c0649180100e456dc51148ad3ab
rimrim990 <62409503+rimrim990@users.noreply.github.com>
2024-01-07 23:47:23 +0900

    fix: 프로세스 종료 스크립트 수정 (#41)

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,6 +4,6 @@ PROJECT_ROOT="/home/ubuntu/app"
 JAR_FILE="$PROJECT_ROOT/build/libs/reflog-server.jar"
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 
-if [ps -p $CURRENT_PID > /dev/null]; then
-    kill -15 $CURRENT_PID
+if [ ${#CURRENT_PID} -gt 0 ]; then
+    kill -15 "$CURRENT_PID"
 fi
