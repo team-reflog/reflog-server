7dc448500bec99ba5dadecfc294f53b2813ababa
whatasame <97666463+whatasame@users.noreply.github.com>
2024-01-12 13:02:47 +0900

    feat: 디버깅 모드에서 예외 로그 출력

--- src/main/java/com/github/teamreflog/reflogserver/auth/exception/AuthExceptionHandler.java
+++ src/main/java/com/github/teamreflog/reflogserver/auth/exception/AuthExceptionHandler.java
@@ -1,18 +1,21 @@
 package com.github.teamreflog.reflogserver.auth.exception;
 
 import com.github.teamreflog.reflogserver.common.exception.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice
 public class AuthExceptionHandler {
 
     @ExceptionHandler(AuthException.class)
     public ResponseEntity<ErrorResponse> handleAuthException(final AuthException e) {
+        log.debug("AuthException: {}", e.getMessage(), e);
 
         return new ResponseEntity<>(new ErrorResponse(e.getMessage()), e.getStatus());
     }
