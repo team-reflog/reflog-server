e32126713c31154a1c76722cc4eb0c74c42ecbef
whatasame <97666463+whatasame@users.noreply.github.com>
2024-01-12 12:27:26 +0900

    refactor: JwtProvider 역할 분리 완료

--- src/main/java/com/github/teamreflog/reflogserver/auth/domain/JwtProvider.java
+++ src/main/java/com/github/teamreflog/reflogserver/auth/domain/JwtProvider.java
@@ -5,8 +5,4 @@ public interface JwtProvider {
     String generateAccessToken(final Long memberId);
 
     String generateRefreshToken(final Long memberId);
-
-    Long parseSubject(final String token);
-
-    String extractToken(final String header);
 }
