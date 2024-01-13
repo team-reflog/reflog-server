c60df1569713529ce8140c8d8f407ae0d8980815
whatasame <97666463+whatasame@users.noreply.github.com>
2024-01-13 23:22:36 +0900

    fix: 댓글 권한 관련 예외 수정

--- src/main/java/com/github/teamreflog/reflogserver/reflection/exception/CommentNotAccessException.java
+++ src/main/java/com/github/teamreflog/reflogserver/reflection/exception/CommentNotAccessException.java
@@ -1,10 +1,10 @@
 package com.github.teamreflog.reflogserver.reflection.exception;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 public class CommentNotAccessException extends CommentException {
 
     public CommentNotAccessException(final Throwable cause) {
-        super(BAD_REQUEST, "댓글을 작성할 수 없습니다.", cause);
+        super(UNAUTHORIZED, "댓글을 작성할 권한이 없습니다.", cause);
     }
 }
