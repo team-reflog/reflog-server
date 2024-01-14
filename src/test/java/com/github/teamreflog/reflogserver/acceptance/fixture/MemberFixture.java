86fd14bfa98cb998bb519a8981abd688799bf5f7
whatasame <97666463+whatasame@users.noreply.github.com>
2024-01-15 01:21:20 +0900

    refactor: 회원 인수 테스트 리팩토링

--- src/test/java/com/github/teamreflog/reflogserver/acceptance/fixture/MemberFixture.java
+++ src/test/java/com/github/teamreflog/reflogserver/acceptance/fixture/MemberFixture.java
@@ -1,8 +1,9 @@
 package com.github.teamreflog.reflogserver.acceptance.fixture;
 
+import static org.hamcrest.Matchers.matchesRegex;
+import static org.springframework.http.HttpHeaders.LOCATION;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-import com.github.teamreflog.reflogserver.member.application.dto.MemberJoinRequest;
 import io.restassured.RestAssured;
 import org.springframework.http.HttpHeaders;
 
@@ -17,14 +18,22 @@ public abstract class MemberFixture {
                 RestAssured.given()
                         .log()
                         .all()
-                        .body(new MemberJoinRequest(email, password))
                         .contentType(APPLICATION_JSON_VALUE)
+                        .body(
+                                """
+                                        {
+                                            "email": "%s",
+                                            "password": "%s"
+                                        }
+                                        """
+                                        .formatted(email, password))
                         .when()
                         .post("/members")
                         .then()
                         .log()
                         .all()
                         .statusCode(201)
+                        .header(LOCATION, matchesRegex("/members/[0-9]+"))
                         .extract()
                         .header(HttpHeaders.LOCATION)
                         .split("/")[2];
