7b0cf002d46d9ffb9b44a512176a3050009bc15f
whatasame <97666463+whatasame@users.noreply.github.com>
2024-01-14 20:35:30 +0900

    feat: 테스트용 인증 번호 생성기 추가 및 인증 메일 인수 테스트 통과

---
+++ src/test/java/com/github/teamreflog/reflogserver/acceptance/fake/FixedMailAuthCodeGenerator.java
@@ -0,0 +1,15 @@
+package com.github.teamreflog.reflogserver.acceptance.fake;
+
+import com.github.teamreflog.reflogserver.mail.domain.MailAuthCodeGenerator;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Primary
+@Component
+public class FixedMailAuthCodeGenerator implements MailAuthCodeGenerator {
+
+    @Override
+    public Integer generateCode() {
+        return 240114;
+    }
+}
