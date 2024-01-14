39b4d815bcf76fcb7240c0c61121778ee7d79730
whatasame <97666463+whatasame@users.noreply.github.com>
2024-01-14 20:32:07 +0900

    refactor: 예외 메시지 수정

--- src/main/java/com/github/teamreflog/reflogserver/mail/exception/MailAuthCodeNotMatchedException.java
+++ src/main/java/com/github/teamreflog/reflogserver/mail/exception/MailAuthCodeNotMatchedException.java
@@ -5,6 +5,6 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 public class MailAuthCodeNotMatchedException extends MailException {
 
     public MailAuthCodeNotMatchedException() {
-        super(BAD_REQUEST, "인증 코드가 일치하지 않습니다.");
+        super(BAD_REQUEST, "인증 번호가 일치하지 않습니다.");
     }
 }
